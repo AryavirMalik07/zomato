a049d3377d1409e190b54a2774141bcbe378d081
AryavirMalik07 <aryavirmalik20@gmail.com>
2023-02-26 23:26:15 +0530

    footer-update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,21 @@
 import React from "react";
 import "../styles/footer.css";
+import logo from "../images/zomato-logo.avif";
+{
+  /* <div className="absolute-center">Made by Aryavir Malik</div>; */
+}
 function Footer() {
-  return <div className="absolute-center">Made by Aryavir Malik</div>;
+  return (
+    <div className="footer">
+      <div className="upper-footer max-width">
+        <img src={logo} className="header-logo" />
+        <div className="region">
+          <button className="footer-btn">India</button>
+          <button className="footer-btn">Language</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
